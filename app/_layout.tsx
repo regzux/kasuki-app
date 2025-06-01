6933c15e525212de0085c2383a1f2253fb4af9c9
Reegie Yorong <regzyorong@gmail.com>
2025-06-01 20:38:05 +0800

    Add login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,63 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider
+} from "react-native-paper";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const theme = {
+  ...DefaultTheme,
+  dark: false,
+  roundness: 2,
+  colors: {
+    primary: "rgb(4, 30, 66)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(206, 220, 240)",
+    onPrimaryContainer: "rgb(0, 18, 41)",
+    secondary: "rgb(255, 199, 44)",
+    onSecondary: "rgb(0, 0, 0)",
+    secondaryContainer: "rgb(255, 236, 166)",
+    onSecondaryContainer: "rgb(60, 48, 0)",
+    tertiary: "rgb(194, 91, 86)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(255, 218, 215)",
+    onTertiaryContainer: "rgb(69, 21, 19)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(255, 251, 255)",
+    onBackground: "rgb(29, 27, 30)",
+    surface: "rgb(255, 251, 255)",
+    onSurface: "rgb(29, 27, 30)",
+    surfaceVariant: "rgb(224, 227, 235)",
+    onSurfaceVariant: "rgb(66, 70, 78)",
+    outline: "rgb(118, 121, 129)",
+    outlineVariant: "rgb(196, 199, 207)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(50, 47, 51)",
+    inverseOnSurface: "rgb(245, 239, 244)",
+    inversePrimary: "rgb(160, 198, 255)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(245, 243, 249)",
+      level2: "rgb(239, 237, 245)",
+      level3: "rgb(232, 235, 241)",
+      level4: "rgb(230, 233, 240)",
+      level5: "rgb(226, 231, 238)"
+    },
+    surfaceDisabled: "rgba(29, 27, 30, 0.12)",
+    onSurfaceDisabled: "rgba(29, 27, 30, 0.38)",
+    backdrop: "rgba(51, 47, 55, 0.4)"
+  }
+};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
   });
 
   if (!loaded) {
@@ -18,12 +66,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <PaperProvider theme={theme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </PaperProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  View
+} from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+
+export default function OnboardingScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require("../assets/images/kasuki-hi-res.png")}
+          style={styles.logo}
+          resizeMode="contain"
+          accessibilityLabel="Kasuki Logo"
+        />
+        <Text variant="headlineLarge">Welcome to Kasuki</Text>
+        <Text variant="headlineSmall" style={styles.subtitle}>
+          Your Suki Rewards, Digitized!
+        </Text>
+        <View style={styles.loginFormContainer}>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={styles.input}
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            style={styles.input}
+          />
+          <Button mode="contained" style={styles.loginButton}>
+            LOGIN
+          </Button>
+        </View>
+        <View style={styles.signupContainer}>
+          <Text variant="bodyMedium">Don't have an account?</Text>
+          <Button mode="elevated" style={styles.signupButton}>
+            Create an account
+          </Button>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 100,
+    paddingHorizontal: 38,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%"
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 36
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  subtitle: {
+    marginBottom: 30
+  },
+  loginFormContainer: {
+    display: "flex",
+    gap: 10,
+    flexDirection: "column",
+    width: "100%"
+  },
+  input: {
+    width: "100%"
+  },
+  loginButton: {
+    height: 50,
+    justifyContent: "center"
+  },
+  signupButton: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center"
+  },
+  signupContainer: {
+    marginTop: 50,
+    display: "flex",
+    gap: 10,
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center"
+  }
+});
